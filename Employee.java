6ed719b293386c6631b7fa20628e43815e7e2d51
Suresh Katari <iamsureshkatari@gmail.com>
2020-05-30 01:51:17 +0530

    Modified employee class

--- Employee.java
+++ Employee.java
@@ -7,9 +7,11 @@ public class Employee {
 	private String email;
 	private Date dateOfBirth;
 	private double salary;
+	private int age;
 	
-	public Employee(String name, String email, Date dateOfBirth, double salary) {
+	public Employee(String name, String email, Date dateOfBirth, int age, double salary) {
 		this.name = name;
+		this.age = age;
 		this.email = email;
 		this.dateOfBirth = dateOfBirth;
 		this.salary = salary;
@@ -21,6 +23,14 @@ public class Employee {
 	public void setName(String name) {
 		this.name = name;
 	}
+
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+
 	public String getEmail() {
 		return email;
 	}
